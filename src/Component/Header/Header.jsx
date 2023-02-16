512730195be63108f664d1c5b501768942a06aa0
Shyam Joshee <skjoshee007@gmail.com>
2023-02-16 20:31:23 +0530

    first comit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from "../../netflixlogo.png"
+import { Link } from 'react-router-dom'
+import { ImSearch} from "react-icons/im"
+import Home from "../Home/Home.jsx"
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="logo" />
+
+
+      <div>
+        <Link to="/tvshows">TV Shows</Link>
+        <Link to="/Movies">Movies</Link>
+        <Link to="/Recently_Added">Recently Added</Link>
+        <Link to="/My_List">My List</Link>
+      </div>
+
+      <ImSearch/>
+
+      
+    </nav>
+    
+
+
+
+
+  )
+}
+
+export default Header
